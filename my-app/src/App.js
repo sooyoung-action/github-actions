5669874ae3d74005cc6c671fb77a1c926f54d12d
1__swim <young940211@naver.com>
2025-11-22 16:48:47 +0900

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn GithubAction ci/cd2 test
+          Learn GithubAction ci/cd3 test
         </a>
       </header>
     </div>
