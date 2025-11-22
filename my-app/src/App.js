d63e4c80ca736e82b3a695347433e81526270c5f
lxx._.hxn <young940211@naver.com>
2025-11-22 18:37:54 +0900

    Add cicd-3.yaml

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn GithubAction version5
+          Learn GithubAction ci/cd3
         </a>
       </header>
     </div>
